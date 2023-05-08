c8b551ae817d9d910e3b877927da79941069bfbb
grassstain <evan.baloney@gmail.com>
2023-05-08 00:39:39 -0300

    eventListeners on each button

---
+++ drum/index.js
@@ -0,0 +1,14 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+
+
+for (let i=0; i<numberOfDrumButtons; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert('I got clicked!');
+    });
+}
+
+    
+
+
